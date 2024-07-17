11a391f174038867bd923dac4dca10acf113b326
TusharikaSh27 <116363877+TusharikaSh27@users.noreply.github.com>
2024-07-17 07:49:42 +0530

    Add files via upload
    a brand page using react

---
+++ src/components/navigation.jsx
@@ -0,0 +1,19 @@
+const Navigation = () => {
+ return (
+        <nav className="container">
+            <div className="logo">
+<img src="/images/brand_logo.png" alt="logo"/>
+            </div>
+            <ul>
+                <li> <a Menu href="#">Menu </a> </li>
+                <li> <a Menu href="#">Location </a> </li>
+                <li> <a Menu href="#">About </a> </li>
+                <li> <a Menu href="#">Contact </a> </li>
+            </ul>
+            
+            <button>Login</button>
+        </nav>
+    );
+};
+
+export default Navigation;
